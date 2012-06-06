0202911af589abe3a19e929e17dbc8ed2d74cb67
ajlopez <webmaster@ajlopez.com>
2012-06-06 18:11:31 -0300

    Subscribe with object instead of predicate

--- test/bus.js
+++ test/bus.js
@@ -11,7 +11,7 @@ exports['Post Message and Subscribe'] = function(test) {
 	test.expect(1);
 	var bus = simplebus.createBus();
 	bus.post("foo");
-	bus.subscribe(function(msg) {
+	bus.subscribe(null, function(msg) {
 		test.equal(msg, "foo");
 		test.done();
 	});
@@ -20,10 +20,35 @@ exports['Post Message and Subscribe'] = function(test) {
 exports['Subscribe and Post Message'] = function(test) {
 	test.expect(1);
 	var bus = simplebus.createBus();
-	bus.subscribe(function(msg) {
+	bus.subscribe(null, function(msg) {
 		test.equal(msg, "foo");
 		test.done();
 	});
 	bus.post("foo");
+}
+
+exports['Subscribe with Predicate and Post Message'] = function(test) {
+	test.expect(1);
+	var bus = simplebus.createBus();
+	bus.subscribe(function(msg) { return msg == "foo" }, function(msg) {
+		test.equal(msg, "foo");
+		test.done();
+	} );
+	bus.post("bar");
+	bus.post("bazinga");
+	bus.post("foo");
+}
+
+exports['Subscribe with Object Example and Post Message'] = function(test) {
+	test.expect(2);
+	var bus = simplebus.createBus();
+	bus.subscribe({ topic: "foo" }, function(msg) {
+		test.equal(msg.topic, "foo");
+		test.equal(msg.amount, 3000);
+		test.done();
+	});
+	bus.post({ topic: "bar", amount: 1000});
+	bus.post({ topic: "bar", amount: 2000});
+	bus.post({ topic: "foo", amount: 3000});
 }
 
