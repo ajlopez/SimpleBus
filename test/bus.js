afb734d4a5ddf3f834ba13f9b49848db9cc6cb31
ajlopez <webmaster@ajlopez.com>
2012-06-06 12:00:23 -0300

    Post and Subscribe, firts tests

--- test/bus.js
+++ test/bus.js
@@ -6,3 +6,24 @@ exports['Create Bus'] = function(test) {
 	test.ok(bus);
     test.done();
 }
+
+exports['Post Message and Subscribe'] = function(test) {
+	test.expect(1);
+	var bus = simplebus.createBus();
+	bus.post("foo");
+	bus.subscribe(function(msg) {
+		test.equal(msg, "foo");
+		test.done();
+	});
+}
+
+exports['Subscribe and Post Message'] = function(test) {
+	test.expect(1);
+	var bus = simplebus.createBus();
+	bus.subscribe(function(msg) {
+		test.equal(msg, "foo");
+		test.done();
+	});
+	bus.post("foo");
+}
+
