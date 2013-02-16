19329af20bf31d1a60c00a70e27c61dc45e4448c
ajlopez <webmaster@ajlopez.com>
2013-02-16 09:33:52 -0300

    Using SimpleFunc 0.0.2, null processing issue fixed in that version

--- test/client.js
+++ test/client.js
@@ -3,13 +3,13 @@ var simplebus = require('../');
 
 exports['Create Server, Client'] = function(test) {
 	var bus = simplebus.createBus();
-    var server = simplebus.createServer(bus, 3000);
+    var server = simplebus.createServer(bus, 3001);
     
     test.expect(0);
     
     server.start();
     
-    var client = simplebus.createClient(3000);
+    var client = simplebus.createClient(3001);
     
     client.start(function () {
         client.stop();
@@ -30,13 +30,36 @@ exports['Post Message from Client to Server'] = function(test) {
 		test.done();
 	});
     
-    var server = simplebus.createServer(bus, 3000);
+    var server = simplebus.createServer(bus, 3001);
         
     server.start();
     
-    var client = simplebus.createClient(3000);
+    var client = simplebus.createClient(3001);
     
     client.start(function () {
         client.post("foo");
+    });
+}
+
+exports['Client Subscribe'] = function(test) {
+	var bus = simplebus.createBus();
+
+    test.expect(1);
+    
+    var server = simplebus.createServer(bus, 3001);
+        
+    server.start();
+    
+    var client = simplebus.createClient(3001);
+    
+    client.start(function () {
+        client.subscribe(null, function (msg) {
+            test.equal(msg, "foo");
+            client.stop();
+            server.stop();
+            test.done();
+        });
+        
+        bus.post("foo");
     });
 }
