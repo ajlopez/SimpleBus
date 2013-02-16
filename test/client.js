47c552969be28a6cdaa068febab291bb4635b1d7
ajlopez <webmaster@ajlopez.com>
2013-02-16 10:23:06 -0300

    Remote client subscription using predicate

--- test/client.js
+++ test/client.js
@@ -64,7 +64,6 @@ exports['Client Subscribe to All Message'] = function(test) {
     });
 }
 
-
 exports['Client Subscribe to Message with Property/Value'] = function(test) {
 	var bus = simplebus.createBus();
 
@@ -78,6 +77,34 @@ exports['Client Subscribe to Message with Property/Value'] = function(test) {
     
     client.start(function () {
         client.subscribe({ operation: 'sale' }, function (msg) {
+            test.ok(msg);
+            test.equal(msg.operation, 'sale');
+            test.equal(msg.price, 20);
+            test.equal(msg.quantity, 1);
+            client.stop();
+            server.stop();
+            test.done();
+        });
+        
+        bus.post({ operation: 'buy', price: 100, quantity: 10 });
+        bus.post({ operation: 'sale', price: 20, quantity: 1 });
+    });
+}
+
+
+exports['Client Subscribe to Message using a Predicate'] = function(test) {
+	var bus = simplebus.createBus();
+
+    test.expect(4);
+    
+    var server = simplebus.createServer(bus, 3001);
+        
+    server.start();
+    
+    var client = simplebus.createClient(3001);
+    
+    client.start(function () {
+        client.subscribe(function (msg) { return msg.price < 50; }, function (msg) {
             test.ok(msg);
             test.equal(msg.operation, 'sale');
             test.equal(msg.price, 20);
